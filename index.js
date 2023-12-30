17b022af28236ffce30cc7db69e27f7f8665c883
Ayush <ayushguptass14@gmail.com>
2023-12-30 20:04:19 +0530

    added4

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import {connect} from './db.js'
+import cors from 'cors';
 const app = express();
 const port = 3002;
-
-
 
 const corsOptions = {
   origin: 'https://6590286b9d7cd172b98d50b4--sensational-concha-ed7566.netlify.app',
 };
 
 app.use(cors(corsOptions));
+
+
 // Define a simple route
 app.get('/hello', (req, res) => {
   res.json({message: 'Hello, Express!'});
